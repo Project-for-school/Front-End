d6e0741755f091023a42d0a7ed26a8348a26baf5
anhkhoi <hoangleanhkhoi1908@gmail.com>
2023-04-05 16:01:57 +0800

    uploaded header footer default

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeaderDefault from "./components/layoutDefault/header/header";
 import HeaderInner from "./components/layoutInner/header/header";
+import FooterDefault from "./components/layoutDefault/footer/footer"
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HeaderDefault />} />
+        {/* <Route path="/" element={<HeaderDefault />} /> */}
         <Route path="/inner" element={<HeaderInner />} />
+        <Route path="" element={<FooterDefault />} />
       </Routes>
     </Router>
+
   );
 }
 
---
+++ src/features/auth/components/homePgae/homePage.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const HomePage = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+}
+
+export default HomePage;
