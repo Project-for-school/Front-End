df8c53bc72cf4c9db26c18e8be67e7270615f945
anhkhoi <hoangleanhkhoi1908@gmail.com>
2023-04-21 12:16:19 +0800

    updated second 21/4

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ import SignUp from "../features/auth/components/register/register";
 import PremiumPage from "../features/premium/components/premium";
 import Demo from "../features/demo/demo";
 import LandingPage from "../features/landingPage/";
+import ExplorePage from "../features/explore/explore";
 
 export const publicRoutes = [
   { path: "/", component: LandingPage, layout: null },
   { path: "/login", component: Login },
+  { path: "/explore", component: ExplorePage },
   { path: "/signup", component: SignUp },
   { path: "/premium", component: PremiumPage },
   { path: "/demo", component: Demo },
